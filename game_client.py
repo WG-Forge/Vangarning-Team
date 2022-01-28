b38e0e28bbf382980a30c48637332212ddb13268
Vasilii Safronov <vks20001@yandex.ru>
2022-01-29 01:04:48 +0300

    Tweaks to GameSession class, added game_loop function

--- game_client.py
+++ game_client.py
@@ -3,48 +3,99 @@ from dotenv import load_dotenv
 
 from server_interaction import Session, ActionCode
 
+# TODO: add docs for GameSession.__init__
+
 load_dotenv()
 HOST = os.getenv("HOST")
 PORT = int(os.getenv("PORT"))
 
 
-class GameSession:
-    def __init__(self, **login_info):
-        self.server = Session(HOST, PORT)
-        self.server.get(ActionCode.LOGIN, login_info)   # Possible errors: no login_info validation
+class WrongPayloadFormatException(Exception):
+    pass
 
+
+def validate_login_info(login_info: dict):
+    valid_fields = (
+        "name",
+        "password",
+        "game",
+        "num_turns",
+        "num_players",
+        "is_observer",
+    )
+
+    for var in login_info.keys():
+        if var not in valid_fields:
+            raise WrongPayloadFormatException(
+                f"Field {var}: {login_info[var]} is not valid login field."
+            )
+
+
+class GameSession:
+    """
+    Handles interaction with server throughout one game session.
+
+    """
+
+    def __init__(self, **login_info,):
+        validate_login_info(login_info)
+
+        self.server = Session(HOST, PORT)
+        login_response = self.server.get(ActionCode.LOGIN, login_info)
+
+        self.current_player_id = login_response["idx"]
         self.map = self.server.get(ActionCode.MAP)
         self.game_state = self.server.get(ActionCode.GAME_STATE)
-
-    def logout(self):
-        self.server.get(ActionCode.LOGOUT)
 
     def update_game_state(self):
         self.game_state = self.server.get(ActionCode.GAME_STATE)
 
-    def game_actions(self):
-        self.server.get(ActionCode.GAME_ACTIONS)
+    # @property
+    # def game_state(self):
+    #     """
+    #     Updates game state from server for every call.
+    #     """
+    #     return self.server.get(ActionCode.GAME_STATE)
 
-    def turn(self):
-        self.server.get(ActionCode.TURN)
+    def game_actions(self) -> dict:
+        return self.server.get(ActionCode.GAME_ACTIONS)
 
-    def chat(self, message: str):
-        self.server.get(ActionCode.CHAT, {"message": message})
+    def turn(self) -> dict:
+        return self.server.get(ActionCode.TURN)
 
-    def move(self, vehicle_id: int, target: dict):
+    def chat(self, message: str) -> dict:
+        return self.server.get(ActionCode.CHAT, {"message": message})
+
+    def move(self, vehicle_id: int, target: dict) -> dict:
+        # TODO: Add validation for vehicle_id and target
         payload = {
             "vehicle_id": vehicle_id,
             "target": target,
         }
-        self.server.get(ActionCode.MOVE, payload)
+        return self.server.get(ActionCode.MOVE, payload)
 
-    def shoot(self, vehicle_id: int, target: dict):
+    def shoot(self, vehicle_id: int, target: dict) -> dict:
+        # TODO: Add validation for vehicle_id and target
         payload = {
             "vehicle_id": vehicle_id,
             "target": target,
         }
-        self.server.get(ActionCode.SHOOT, payload)
+        return self.server.get(ActionCode.SHOOT, payload)
+
+    def logout(self) -> dict:
+        return self.server.get(ActionCode.LOGOUT)
 
 
-if __name__ == '__main__':
-    pass
+def game_loop(game: GameSession):
+    while True:
+        game.update_game_state()
+        game_state = game.game_state
+        if game_state["current_player_idx"] == game.current_player_id:
+            pass  # Send game_state to bot
+
+        game.turn()
+
+
+if __name__ == "__main__":
+    game_session = GameSession()
+    game_loop(game_session)
