5a5e0ec180b9aca06097a68bd219a9e0d36f0182
Vasilii Safronov <vks20001@yandex.ru>
2022-01-28 15:44:50 +0300

    Reformatted file

--- server_interaction.py
+++ server_interaction.py
@@ -26,7 +26,7 @@ class Session:
     def __init__(self, host: str, port: int):
         self.server_details = (host, port)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.client_socket.setblocking(False)
+        self.client_socket.settimeout(11)
         self.client_socket.connect(self.server_details)
 
     def __del__(self):
@@ -34,12 +34,11 @@ class Session:
 
     def _generate_message(self, code: int, data: Optional[dict] = None):
         if data is None:
-            return struct.pack('<ii', code, 0)
+            return struct.pack("<ii", code, 0)
 
         data_length = len(str(data))
         return struct.pack(
-            f'<ii{data_length}s',
-            code, data_length, json.dumps(data).encode('utf-8')
+            f"<ii{data_length}s", code, data_length, json.dumps(data).encode("utf-8")
         )
 
     def get(self, action: (ActionCode, int), data: Optional[dict] = None) -> dict:
@@ -49,32 +48,28 @@ class Session:
         message = self._generate_message(action, data)
         self.client_socket.sendall(message)
 
-        response_code = int.from_bytes(
-            self.client_socket.recv(4),
-            'little'
-        )
-        received_data_len = int.from_bytes(
-            self.client_socket.recv(4),
-            'little'
-        )
-        received_data = {} if received_data_len == 0 else json.loads(
-            self.client_socket.recv(received_data_len)
+        response_code = int.from_bytes(self.client_socket.recv(4), "little")
+        received_data_len = int.from_bytes(self.client_socket.recv(4), "little")
+        received_data = (
+            {}
+            if received_data_len == 0
+            else json.loads(self.client_socket.recv(received_data_len))
         )
 
         result = {
-            'response_code': response_code,
-            'data': received_data,
+            "response_code": response_code,
+            "data": received_data,
         }
         return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     from dotenv import load_dotenv
 
     load_dotenv()
-    HOST = os.getenv('HOST')
-    PORT = int(os.getenv('PORT'))
+    HOST = os.getenv("HOST")
+    PORT = int(os.getenv("PORT"))
 
     s = Session(HOST, PORT)
     login_response = s.get(ActionCode.LOGIN, {"name": "Boris"})
