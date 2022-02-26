b00c279844d5bbb195c513547ad67e85ace66348
liquidGoo <nitor25@gmail.com>
2022-02-26 09:20:31 +0300

    Added new hex support to gui

--- WoT_Strategy/gui.py
+++ WoT_Strategy/gui.py
@@ -8,12 +8,17 @@ os.environ["KIVY_NO_ARGS"] = "1"
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.properties import ListProperty, NumericProperty, ObjectProperty
+from kivy.properties import (
+    ListProperty,
+    NumericProperty,
+    ObjectProperty,
+    ReferenceListProperty,
+)
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scatter import Scatter
 from kivy.uix.widget import Widget
 
-from settings import CoordsTuple
+from settings import CoordsTuple, CoordsDict
 from game_client import game_tick
 from hex import tuple_to_dict, dict_to_tuple
 
@@ -32,6 +37,12 @@ VEHICLE_TYPES_TO_SPRITES = {
     "medium_tank": "assets/MT.png",
     "spg": "assets/SPG.png",
 }
+SPECIAL_HEXES_TO_SPRITES = {
+    "light_repair": "assets/light_repair.png",
+    "hard_repair": "assets/hard_repair.png",
+    "catapult": "assets/catapult.png",
+}
+CONSUMABLES_MAX_USES = {"light_repair": -1, "hard_repair": -1, "catapult": 3}
 
 
 def cube_to_cartesian(coords: CoordsTuple) -> tuple[float, float]:
@@ -84,25 +95,101 @@ class HPBar(Widget):
             i -= 1
 
 
-class Vehicle(Widget):
+class Entity(Widget):
+    """
+    Widget for independently positioned entity
+    """
+
+    x_coord = NumericProperty(0)
+    y_coord = NumericProperty(0)
+    coords = ReferenceListProperty(x_coord, y_coord)
+    color = ListProperty([0, 0, 0])
+
+
+class Vehicle(Entity):
     """
     Vehicle widget
     """
 
+    file: str
     hp_bar = ObjectProperty(0)
-    color = ListProperty([0, 0, 0])
-    file = ""
 
 
-class Hex(Widget):
+class Hex(Entity):
     """
     Hex widget
     """
 
     side = NumericProperty(0)
-    color = ListProperty([0, 0, 0])
-    x_coord = NumericProperty(0)
-    y_coord = NumericProperty(0)
+
+
+class SpecialHex(Entity):
+    """
+    Special hex widget
+    """
+
+    file: str
+    uses = NumericProperty(0)
+    type = ""
+
+    def on_uses(self, _instance, value):
+        """
+        Callback for updating consumable
+        :param _instance:
+        :param value:
+        :return:
+        """
+        max_uses = CONSUMABLES_MAX_USES[self.type]
+        if max_uses == -1:
+            return
+
+        if value >= max_uses:
+            self.color = COLORS["black"]
+            self.opacity = 0.2
+        else:
+            self.color = COLORS["white"]
+            self.opacity = 1
+
+
+def create_hexes(map_data: dict):
+    """
+    Hex widgets generator
+    :param map_data: Map dict
+    :return:
+    """
+    for cube_x in range(-map_data["size"], map_data["size"] + 1):
+        for cube_y in range(
+            max(-map_data["size"], -map_data["size"] - cube_x),
+            min(map_data["size"] + 1, map_data["size"] - cube_x + 1),
+        ):
+            cube_coords_tuple = (cube_x, cube_y, -cube_x - cube_y)
+            new_hex = Hex()
+
+            new_hex.coords = cube_to_cartesian(cube_coords_tuple)
+
+            color = COLORS["grey"]
+            cube_coords_dict = tuple_to_dict(cube_coords_tuple)
+            if cube_coords_dict in map_data["content"]["base"]:
+                color = COLORS["white"]
+            if cube_coords_dict in map_data["content"]["obstacle"]:
+                color = COLORS["black"]
+            new_hex.color = color
+
+            yield new_hex
+
+
+def create_special_hex(cube_coords: CoordsDict, hex_type: str):
+    """
+    Creates special hex widget
+    :param cube_coords: Cube coordinates of hex in dict
+    :param hex_type: Type of hex
+    :return:
+    """
+    new_hex = SpecialHex()
+    new_hex.file = SPECIAL_HEXES_TO_SPRITES[hex_type]
+    new_hex.coords = cube_to_cartesian(dict_to_tuple(cube_coords))
+    new_hex.type = hex_type
+    return new_hex
 
 
 class WoTStrategyRoot(Widget):
@@ -111,6 +198,7 @@ class WoTStrategyRoot(Widget):
     """
 
     vehicles: dict[int, Vehicle] = {}
+    consumables: dict[tuple[int, int, int], SpecialHex] = {}
     not_used_colors = [COLORS["red"], COLORS["green"], COLORS["blue"]]
     ids_to_colors: dict[int, tuple[int, int, int]] = {}
     scatter = Scatter()
@@ -119,7 +207,7 @@ class WoTStrategyRoot(Widget):
         """
         Creates vehicle widget
         :param vehicle_id:
-        :param vehicle_data: Vehicle data dict
+        :param vehicle_data: Vehicle dict
         :return:
         """
         if vehicle_data["player_id"] not in self.ids_to_colors:
@@ -143,27 +231,21 @@ class WoTStrategyRoot(Widget):
         :param map_data: Map dict
         :return:
         """
-        for cube_x in range(-map_data["size"], map_data["size"] + 1):
-            for cube_y in range(
-                max(-map_data["size"], -map_data["size"] - cube_x),
-                min(map_data["size"] + 1, map_data["size"] - cube_x + 1),
-            ):
-                cube_coords_tuple = (cube_x, cube_y, -cube_x - cube_y)
-                new_hex = Hex()
+        for new_hex in create_hexes(map_data):
+            self.scatter.add_widget(new_hex)
 
-                cartesian_x, cartesian_y = cube_to_cartesian(cube_coords_tuple)
-                new_hex.x_coord = cartesian_x
-                new_hex.y_coord = cartesian_y
-
-                color = COLORS["grey"]
-                cube_coords_dict = tuple_to_dict(cube_coords_tuple)
-                if cube_coords_dict in map_data["content"]["base"]:
-                    color = COLORS["white"]
-                if cube_coords_dict in map_data["content"]["obstacle"]:
-                    color = COLORS["black"]
-                new_hex.color = color
-
-                self.scatter.add_widget(new_hex)
+        for light_repair_cube_coords in map_data["content"]["light_repair"]:
+            self.scatter.add_widget(
+                create_special_hex(light_repair_cube_coords, "light_repair")
+            )
+        for hard_repair_cube_coords in map_data["content"]["hard_repair"]:
+            self.scatter.add_widget(
+                create_special_hex(hard_repair_cube_coords, "hard_repair")
+            )
+        for catapult_cube_coords in map_data["content"]["catapult"]:
+            self.scatter.add_widget(
+                create_special_hex(catapult_cube_coords, "catapult")
+            )
 
     def size_setter(self, _instance, width, height):
         """
@@ -175,11 +257,20 @@ class WoTStrategyRoot(Widget):
         """
         self.scatter.pos = (width / 2, height / 2)
 
-    def update_vehicles(self, vehicles_data):
+    def update(self, game_state: dict):
+        """
+        Updates gui
+        :param game_state: Game state dict
+        :return:
+        """
+        self.update_vehicles(game_state["vehicles"])
+        self.update_special_hexes(game_state)
+
+    def update_vehicles(self, vehicles_data: dict):
         """
         Updates vehicle widgets
 
-        :param vehicles_data: Vehicle data dict
+        :param vehicles_data: List of vehicle dicts
         :return:
         """
         for id_string, vehicle_data in vehicles_data.items():
@@ -194,6 +285,18 @@ class WoTStrategyRoot(Widget):
 
             vehicle.hp_bar.hp = vehicle_data["health"]
 
+    def update_special_hexes(self, game_state: dict):
+        """
+        Updates special hexes
+        :param game_state: Game state dict
+        :return:
+        """
+        for consumable in self.consumables.values():
+            consumable.uses = 0
+        for hex_cube_coords in game_state["catapult_usage"]:
+            hex_cube_coords_tuple = dict_to_tuple(hex_cube_coords)
+            self.consumables[hex_cube_coords_tuple].uses += 1
+
 
 class WoTStrategyApp(App):
     """
@@ -203,8 +306,8 @@ class WoTStrategyApp(App):
     def __init__(self, map_data, bot, game_session):
         """
 
-        :param map_data: map dict from server
-        :param bot: bot that will be used to choose actions
+        :param map_data: Map dict from server
+        :param bot: Bot that will be used to choose actions
         :param game_session:
         """
         super().__init__()
@@ -233,5 +336,5 @@ class WoTStrategyApp(App):
         new_state = game_tick(self.bot, self.game_session)
         if new_state is None:
             return
-        self.root.update_vehicles(new_state["vehicles"])
+        self.root.update(new_state)
         Clock.schedule_once(self.tick, 0)
