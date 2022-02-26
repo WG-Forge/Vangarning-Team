6714f3ac2a698a8dd6edd0a6ca5472e0c39351da
liquidGoo <nitor25@gmail.com>
2022-02-26 07:39:40 +0300

    Gui codestyple fixes

--- WoT_Strategy/gui.py
+++ WoT_Strategy/gui.py
@@ -1,24 +1,29 @@
+"""
+Handles graphic interface
+"""
+
 import os
 
 os.environ["KIVY_NO_ARGS"] = "1"
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.graphics import Color, Rectangle
 from kivy.properties import ListProperty, NumericProperty, ObjectProperty
-from kivy.uix.widget import Widget
-from kivy.uix.scatter import Scatter
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scatter import Scatter
+from kivy.uix.widget import Widget
 
+from settings import CoordsTuple
 from game_client import game_tick
+from hex import tuple_to_dict, dict_to_tuple
 
 COLORS = {
-    "black": [0, 0, 0],
-    "grey": [0.5, 0.5, 0.5],
-    "white": [1, 1, 1],
-    "red": [1, 0, 0],
-    "green": [0, 1, 0],
-    "blue": [0, 0, 1],
+    "black": (0, 0, 0),
+    "grey": (0.5, 0.5, 0.5),
+    "white": (1, 1, 1),
+    "red": (1, 0, 0),
+    "green": (0, 1, 0),
+    "blue": (0, 0, 1),
 }
 VEHICLE_TYPES_TO_SPRITES = {
     "at_spg": "assets/AT_SPG.png",
@@ -29,67 +34,95 @@ VEHICLE_TYPES_TO_SPRITES = {
 }
 
 
-def cube_to_cartesian(x, y, z):
-    return x * 1.5, (y - z) * 0.866025
+def cube_to_cartesian(coords: CoordsTuple) -> tuple[float, float]:
+    """
+    Translates hex coordinates from cube to cartesian
+    :param coords: cube coordinates tuple
+    :return: cartesian coordinates tuple
+    """
+    return coords[0] * 1.5, (coords[1] - coords[2]) * 0.866025
 
 
 class SingleHPBar(Widget):
-    pass
+    """
+    Hp widget
+    """
 
 
 class HPBar(Widget):
+    """
+    Hp bar widget
+    """
+
     max_hp = NumericProperty(0)
     hp = NumericProperty(0)
     offset = NumericProperty(0)
     layout = BoxLayout()
 
-    def on_max_hp(self, instance, value):
+    def on_max_hp(self, _instance, value):
+        """
+        Callback for creating hp widgets in hp bar
+        :param _instance: Event dispatcher, not used
+        :param value: new max_hp value
+        :return:
+        """
         self.layout.clear_widgets()
-        for i in range(value):
+        for _ in range(value):
             self.layout.add_widget(SingleHPBar())
 
-    def on_hp(self, instance, value):
+    def on_hp(self, _instance, value):
+        """
+        Callback for changing displayed hp widgets in hp bar
+
+        :param _instance: Event dispatcher, not used
+        :param value: new hp value
+        :return:
+        """
         i = self.max_hp
         for bar in self.layout.children:
             bar.opacity = 1 if i <= value else 0
             i -= 1
 
-    def update(self, *args):
-        self.canvas.clear()
-        with self.canvas:
-            Color(1, 1, 1, 0.5)
-            Rectangle(pos=self.pos, size=self.size)
-            Color(0, 1, 0, 1)
-            for i in range(self.hp):
-                Rectangle(
-                    pos=(
-                        self.x
-                        + self.outline_width
-                        + i * (self.single_bar_size[0] + self.outline_width),
-                        self.y + self.outline_width,
-                    ),
-                    size=self.single_bar_size,
-                )
-
 
 class Vehicle(Widget):
+    """
+    Vehicle widget
+    """
+
     hp_bar = ObjectProperty(0)
     color = ListProperty([0, 0, 0])
+    file = ""
 
 
 class Hex(Widget):
+    """
+    Hex widget
+    """
+
     side = NumericProperty(0)
     color = ListProperty([0, 0, 0])
+    x_coord = NumericProperty(0)
+    y_coord = NumericProperty(0)
 
 
 class WoTStrategyRoot(Widget):
-    vehicles = {}
+    """
+    Root widget for app
+    """
+
+    vehicles: dict[int, Vehicle] = {}
     not_used_colors = [COLORS["red"], COLORS["green"], COLORS["blue"]]
-    ids_to_colors = {}
+    ids_to_colors: dict[int, tuple[int, int, int]] = {}
     scatter = Scatter()
 
-    def add_vehicle(self, id, vehicle_data):
-        if vehicle_data["player_id"] not in self.ids_to_colors.keys():
+    def add_vehicle(self, vehicle_id, vehicle_data):
+        """
+        Creates vehicle widget
+        :param vehicle_id:
+        :param vehicle_data: Vehicle data dict
+        :return:
+        """
+        if vehicle_data["player_id"] not in self.ids_to_colors:
             self.ids_to_colors[vehicle_data["player_id"]] = self.not_used_colors[0]
             self.not_used_colors.remove(self.not_used_colors[0])
 
@@ -100,46 +133,62 @@ class WoTStrategyRoot(Widget):
         vehicle.color = self.ids_to_colors[vehicle_data["player_id"]]
         vehicle.file = VEHICLE_TYPES_TO_SPRITES[vehicle_data["vehicle_type"]]
 
-        self.vehicles[id] = vehicle
+        self.vehicles[vehicle_id] = vehicle
         self.scatter.add_widget(vehicle)
 
     def create_map(self, map_data: dict):
-        for x in range(-map_data["size"], map_data["size"] + 1):
-            for y in range(
-                    max(-map_data["size"], -map_data["size"] - x),
-                    min(map_data["size"] + 1, map_data["size"] - x + 1),
-            ):
-                z = -x - y
-                hex = Hex()
+        """
+        Creates hex widgets
 
-                pos_x, pos_y = cube_to_cartesian(x, y, z)
-                hex.x_coord = pos_x
-                hex.y_coord = pos_y
+        :param map_data: Map dict
+        :return:
+        """
+        for cube_x in range(-map_data["size"], map_data["size"] + 1):
+            for cube_y in range(
+                max(-map_data["size"], -map_data["size"] - cube_x),
+                min(map_data["size"] + 1, map_data["size"] - cube_x + 1),
+            ):
+                cube_coords_tuple = (cube_x, cube_y, -cube_x - cube_y)
+                new_hex = Hex()
+
+                cartesian_x, cartesian_y = cube_to_cartesian(cube_coords_tuple)
+                new_hex.x_coord = cartesian_x
+                new_hex.y_coord = cartesian_y
 
                 color = COLORS["grey"]
-                hex_cube = {"x": x, "y": y, "z": z}
-                if hex_cube in map_data["content"]["base"]:
+                cube_coords_dict = tuple_to_dict(cube_coords_tuple)
+                if cube_coords_dict in map_data["content"]["base"]:
                     color = COLORS["white"]
-                if hex_cube in map_data["content"]["obstacle"]:
+                if cube_coords_dict in map_data["content"]["obstacle"]:
                     color = COLORS["black"]
-                hex.color = color
+                new_hex.color = color
 
-                self.scatter.add_widget(hex)
+                self.scatter.add_widget(new_hex)
 
-    def size_setter(self, instance, width, height):
+    def size_setter(self, _instance, width, height):
+        """
+        Callback for setting position of the scatter widget depending on the size of the window
+        :param _instance: Event dispatcher
+        :param width: Window width
+        :param height: Window height
+        :return:
+        """
         self.scatter.pos = (width / 2, height / 2)
 
     def update_vehicles(self, vehicles_data):
-        for id, vehicle_data in vehicles_data.items():
+        """
+        Updates vehicle widgets
 
-            if id not in self.vehicles:
-                self.add_vehicle(id, vehicle_data)
-            vehicle = self.vehicles[id]
+        :param vehicles_data: Vehicle data dict
+        :return:
+        """
+        for id_string, vehicle_data in vehicles_data.items():
+            vehicle_id = int(id_string)
+            if vehicle_id not in self.vehicles:
+                self.add_vehicle(vehicle_id, vehicle_data)
+            vehicle = self.vehicles[vehicle_id]
 
-            pos_dict = vehicle_data["position"]
-            pos_x, pos_y = cube_to_cartesian(
-                pos_dict["x"], pos_dict["y"], pos_dict["z"]
-            )
+            pos_x, pos_y = cube_to_cartesian(dict_to_tuple(vehicle_data["position"]))
             vehicle.x_coord = pos_x
             vehicle.y_coord = pos_y
 
@@ -174,7 +223,13 @@ class WoTStrategyApp(App):
         Clock.schedule_once(self.tick, 0)
         return root
 
-    def tick(self, dt):
+    def tick(self, _dt):
+        """
+        Performs single tick with gui
+
+        :param _dt: Time since last frame, not used
+        :return:
+        """
         new_state = game_tick(self.bot, self.game_session)
         if new_state is None:
             return
