fb0d7f1ef4389d4e35411533b85e6aa595a9028e
anya.konkolovich <anya.konkolovich@gmail.com>
2022-03-08 22:33:00 +0300

    Added class for game state logger

--- WoT_Strategy/game_client.py
+++ WoT_Strategy/game_client.py
@@ -417,3 +417,33 @@ def game_loop(bot: Bot, game: GameSession):
                 )
 
         game.turn()
+
+
+def game_tick(bot: Bot, game: GameSession):
+    """
+    Performs full turn in the game
+    :param bot:
+    :param game:
+    :return:
+    """
+    game_state = game.game_state()
+    game.get_game_state_logger().log_game_state(game_state)
+
+    if game_state["finished"]:
+        print("You won" if game_state["winner"] == game.player_id else "You lost")
+        print(f"Winner: {game_state['winner']}")
+        return None
+
+    if game_state["current_player_idx"] == game.player_id:
+        print(f'Round: {game_state["current_turn"]}, ' f"player: {game.player_name}")
+        for action in bot.get_actions(game_state):
+            game.action(*action.server_format)
+            print(
+                f"  Action: "
+                f'{"shoot" if action.action_code == ActionCode.SHOOT else "move"}'
+                f" Actor: {action.actor} Target: {action.target}"
+            )
+
+    game.turn()
+
+    return game_state
